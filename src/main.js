6edc747cbef0211e85f812060eafe00f5bc922ca
hexbreak <theownerproject@gmail.com>
2020-12-19 06:00:55 +0000

    Updates

--- src/main.js
+++ src/main.js
@@ -4,32 +4,34 @@ import "./style.css";
 
 // import "./assets/img/4geeks.ico";
 
-//window.onload = function() {
-//  console.log("Hello Rigo from the console!");
-//};
+/*
+window.onload = function() {
+  console.log("Hello Rigo from the console!");
+};
+*/
 
-//function cardDealer() {
-//  let tLayer = ["&#9674", "&#9824", "&#9827", "&#9829", "&#9830"];
-//  let bLayer = ["&#9674", "&#9824", "&#9827", "&#9829", "&#9830"];
-//  let rNumber = [
-//    "A",
-//    "2",
-//    "3",
-//    "4",
-//    "5",
-//    "6",
-//    "7",
-//    "8",
-//    "9",
-//    "10",
-//    "J",
-//    "Q",
-//    "K"
-//  ];
-
+/* function cardDealer() {
+  let tLayer = ["&#9674", "&#9824", "&#9827", "&#9829", "&#9830"];
+  let bLayer = ["&#9674", "&#9824", "&#9827", "&#9829", "&#9830"];
+  let rNumber = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+   "K"
+  ];
+*/
 document.querySelector(".top-layer").innerHTML = "&#9824";
 
 document.querySelector(".numbers").innerHTML = "5";
 
 document.querySelector(".bottom-layer").innerHTML = "&#9824";
-// }
+/* } */
