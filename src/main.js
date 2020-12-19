81a558c508130c3b23b44211944de0da20926980
hexbreak <theownerproject@gmail.com>
2020-12-19 03:22:34 +0000

    Updates

--- src/main.js
+++ src/main.js
@@ -4,14 +4,32 @@ import "./style.css";
 
 // import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
-};
+//window.onload = function() {
+//  console.log("Hello Rigo from the console!");
+//};
 
-function cardDealer() {
-  document.querySelector(".top-layer").innerHTML = "&#9824";
+//function cardDealer() {
+//  let tLayer = ["&#9674", "&#9824", "&#9827", "&#9829", "&#9830"];
+//  let bLayer = ["&#9674", "&#9824", "&#9827", "&#9829", "&#9830"];
+//  let rNumber = [
+//    "A",
+//    "2",
+//    "3",
+//    "4",
+//    "5",
+//    "6",
+//    "7",
+//    "8",
+//    "9",
+//    "10",
+//    "J",
+//    "Q",
+//    "K"
+//  ];
 
-  document.querySelector(".numbers").innerHTML = "5";
+document.querySelector(".top-layer").innerHTML = "&#9824";
 
-  document.querySelector(".bottom-layer").innerHTML = "&#9824";
-}
+document.querySelector(".numbers").innerHTML = "5";
+
+document.querySelector(".bottom-layer").innerHTML = "&#9824";
+// }
